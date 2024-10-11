326ab4d7eb165a586b9853444a80e43a83346c40
Tinadin <pitynia8717@gmail.com>
2024-10-11 23:24:42 +0300

    update all

--- js/modal.js
+++ js/modal.js
@@ -6,13 +6,23 @@
     closeModalBtn: document.querySelector("[data-modal-close]"),
     // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector("[data-modal]"),
+    menu: document.querySelector("[data-menu]"),
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]")
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+   refs.closeMenuBtn.addEventListener("click", toggleMenu)
+
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle("is-open");
+  }
+  function toggleMenu() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.menu.classList.toggle("is-open");
   }
 })();
